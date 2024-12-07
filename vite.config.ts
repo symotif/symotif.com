084226a2bf49aaf947f296eea207d2615e4ed879
loganynguyen <logan.y.nguyen@gmail.com>
2024-12-07 15:32:04 -0500

    github actions deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	base: "/symotif.com/",
 	ssr: {
 		noExternal: ['three', 'troika-three-text'],
 	},
