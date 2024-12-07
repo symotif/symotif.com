af39db0e1591ec10d6e438598d8cfa2b744153fe
loganynguyen <logan.y.nguyen@gmail.com>
2024-12-07 15:49:01 -0500

    removed base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	base: "/symotif.com/",
 	ssr: {
 		noExternal: ['three', 'troika-three-text'],
 	},
